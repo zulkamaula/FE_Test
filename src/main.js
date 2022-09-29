b9ba2560e4aaec00f38482a22f961cc612d8d4f6
zulkamaula <mzulkariski@gmail.com>
2022-09-29 08:52:48 +0700

    fe-feat(home): create new Home, adding bootstrap4 and bootstrap-vue with vue-app

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  BootstrapVue,
+  IconsPlugin
+} from "bootstrap-vue";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="border container-fluid">
+        Hello from HOME!
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
