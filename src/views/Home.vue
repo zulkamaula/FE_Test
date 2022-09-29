660e0dc17f33e993b3be3da91827a6ecb0e33c50
zulkamaula <mzulkariski@gmail.com>
2022-09-29 15:05:58 +0700

    fe-feat(structure): adding and split section for graph and gallery

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,49 @@
 <template>
-    <div class="border container-fluid">
-        Hello from HOME!
+    <div class="container-fluid">
+        <div class="row border justify-content-center py-3">
+            <div class="col-2 pt-3 pr-0 align-self-start">
+                <div class="border p-2">
+                    Home
+                </div>
+                <div class="border p-2">
+                    Master Ruas
+                </div>
+            </div>
+            <div class="col-9 py-3 border rounded-lg">
+                <div class="container-fluid">
+                    <section class="graph-section">
+                        <h4 class="mb-4">
+                            Home
+                        </h4>
+    
+                        <div class="row mt-4">
+                            <div class="col-4">
+                                <div class="border just-box text-center">
+                                    Grafik
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="border just-box text-center">
+                                    Bar Chart
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="border just-box text-center">
+                                    Line Chart
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                    <section class="gallery-section">
+                        <h5 class="mt-4">
+                            Image Gallery
+                        </h5>
+                    </section>
+
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,6 +53,6 @@
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    @import '../assets/css/style-guide.css';
 </style>
