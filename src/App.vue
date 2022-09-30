88b40ac00a20bb95aa8a7b315d5686469e2a4228
zulkamaula <mzulkariski@gmail.com>
2022-09-30 13:51:10 +0700

    fe-feat(structure): adding background, transition, and condition change section

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,10 @@ export default {
 </script>
 
 <style>
+  #app {
+    margin: 0;
+    padding: 0;
+    background: rgb(207,212,236);
+background: radial-gradient(circle, rgba(207,212,236,1) 0%, rgba(89,89,89,1) 100%);
+  }
 </style>
